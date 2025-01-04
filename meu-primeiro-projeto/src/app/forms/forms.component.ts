fea48b20d7c2b45a16418a6ee895b1ed896d509d
PedroHCN <pedro.hcorradini05@gmail.com>
2025-01-03 22:37:45 -0300

    ngForm + selected/errors and ngSubmite

--- meu-primeiro-projeto/src/app/forms/forms.component.ts
+++ meu-primeiro-projeto/src/app/forms/forms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-forms',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormsComponent implements OnInit {
 
+  public listaVagas: Array<{vaga: string}> = [
+    {vaga: "Infraestrutura de TI"},
+    {vaga: "Desenvolvimento"},
+    {vaga: "Seguranda da Informação"}
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public submitForm(form: NgForm){
+    console.log(form);
   }
 
 }
